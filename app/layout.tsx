7f178ef1a5b151f0386a3ef57a2ac8034c4ed58b
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2025-01-07 19:40:51 -0800

    installed radix UI, Build issue page, input field, text area, validation with zod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import './theme-config.css'
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ["latin"],
+  variable: "--font-inter", 
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,9 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+      <body className={inter.variable}>
+        
+      <Theme accentColor="iris" >
+       
+          <NavBar />
+          <main className="p-5">{children}</main>
+      
+        </Theme>
       </body>
     </html>
   );
