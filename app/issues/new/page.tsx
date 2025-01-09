3a7501652b3198a928694694cfbed67a9a3d10c0
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2025-01-08 20:02:44 -0800

    Add React Hook forms, Add Handle potential errors,Clinet side validataion,Add  ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,30 +1,65 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 interface IssueForm {
   title: string;
   description: string;
 }
-                                                                                                                                                                                                              
+
 const NewIssuePage = () => {
-  const { register, control } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
+  const [error, setError] = useState("");
+
+  const router = useRouter();
 
   return (
     <>
-      <div className="max-w-xl space-y-3">
-        <TextField.Root size="2" placeholder="title" {...register("title")}>
+      <div className="max-w-xl">
+        {error && (
+          <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+        <form
+          className=" space-y-3"
+          onSubmit={handleSubmit(async (data) => {
+            try {
+              await axios.post("/api/issues", data);
+              router.push("/issues");
+            } catch (error) {
+              setError("An unexpected error has occurred");
+            }
+          })}
+        >
+          <TextField.Root size="2" placeholder="title" {...register("title")} />
+          <ErrorMessage>{errors.title?.message}</ErrorMessage>
           <Controller
             name="description"
             control={control}
-            render={(field) => <SimpleMDE placeholder="description" {...field}/>}
+            render={({ field }) => (
+              <SimpleMDE placeholder="description" {...field} />
+            )}
           />
-        </TextField.Root>
-
-        <Button>Submit New Issue</Button>
+          <ErrorMessage>{errors.description?.message}</ErrorMessage>
+          <Button>Submit New Issue</Button>
+        </form>
       </div>
     </>
   );
