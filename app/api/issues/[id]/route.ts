4ddb39437bbe7bdc79705fe23368a227de9d12ae
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2025-01-15 20:08:37 -0800

    Delete issue,Dialog box, API for Delete, handle error

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -28,11 +28,23 @@ export async function PATCH(request:NextRequest,{params}: {params:{id:string}}) 
         });
 
         return NextResponse.json(updateIssue);
+}
 
 
 
+export async function DELETE(request:NextRequest,{params}: {params:{id:string}}) {
 
+ const issue =  await prisma.issue.findUnique({
+        where: {id: parseInt(params.id)}
+    });
 
+    if(!issue)
+        return NextResponse.json({error:"Invalid issue"}, {status: 400})
 
+    await prisma.issue.delete({
+        where:{id: issue.id}
+    })
+
+    return NextResponse.json({});
 
 }
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,62 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
-const DeleteIssueButton = () => {
+const DeleteIssueButton = ({issueId}:{issueId:number}) => {
+
+    const router = useRouter();
+    const [error, setError] = useState(false)
   return (
    <>
-    <Button color='red'>Delete Issue</Button>
+  <AlertDialog.Root>
+	<AlertDialog.Trigger>
+		<Button color="red">Delete</Button>
+	</AlertDialog.Trigger>
+	<AlertDialog.Content maxWidth="450px">
+		<AlertDialog.Title>Delete Issue</AlertDialog.Title>
+		<AlertDialog.Description size="2">
+	This will delete FOR EVER!!!!
+		</AlertDialog.Description>
+
+		<Flex gap="3" mt="4" justify="end">
+			<AlertDialog.Cancel>
+				<Button variant="soft" color="gray">
+					Cancel
+				</Button>
+			</AlertDialog.Cancel>
+			<AlertDialog.Action>
+				<Button variant="solid" color="red" onClick={() => {
+          try {
+           
+            axios.delete('/api/issues/' + issueId);
+            router.push('/issues')
+            router.refresh();
+            
+          } catch (error) {
+            setError(true);
+          }
+        }}>
+					Delete Issue
+				</Button>
+			</AlertDialog.Action>
+		</Flex>
+	</AlertDialog.Content>
+</AlertDialog.Root>
+
+<AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+            <AlertDialog.Title>
+              Error
+            </AlertDialog.Title>
+            <AlertDialog.Description>
+              This issue could not be deleted
+            </AlertDialog.Description>
+            <Button className='mt-2' color='gray' variant='soft' onClick={()=> setError(false)}>OK</Button>
+        </AlertDialog.Content>
+</AlertDialog.Root>
+
    </>
   )
 }
