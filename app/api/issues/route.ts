29128c9bba7b25e43491f5515f45d39749480bb3
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2025-01-22 17:31:13 -0800

    Finally fixed POST error

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,8 @@
-import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "@/app/validationSchemas";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 
 export async function POST(request: NextRequest) {
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest) {
      const sessions = await getServerSession(authOptions)
 
      if(!sessions) 
-        return NextResponse.json({}),{status: 401};
+        return NextResponse.json({},{status: 401});
     
     const body = await request.json()
 
@@ -25,3 +25,27 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(newIssue, { status: 201 });
 
 }
+// import { prisma } from "@/prisma/client";
+// import { NextRequest, NextResponse } from "next/server";
+// import { issueSchema } from "../../validationSchemas";
+// import { getServerSession } from "next-auth";
+// import authOptions from "@/app/auth/authOptions";
+
+// export async function POST(request: NextRequest) {
+
+//     const session = await getServerSession(authOptions);
+
+//     if(!session)
+//         return NextResponse.json({},{status: 401});
+
+//     const body = await request.json();
+//     const validation = issueSchema.safeParse(body);
+//     if (!validation.success)
+//         return NextResponse.json(validation.error.format(), { status: 400 })
+
+//     const newIssue = await prisma.issue.create({
+//         data: { title: body.title, description: body.description }
+//     });
+
+//     return NextResponse.json(newIssue, { status: 201 });
+// }
